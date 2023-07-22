1bc9a86725681a3557558d10f15baef44bdcc06d
清水稜也 <ryoya0111g.o.l.f@gmail.com>
2023-07-22 19:04:21 +0900

    基本構文の練習

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
+import { type } from 'os';
 import React from 'react';
 
+
+
 const App: React.FC = () => {
+  const message: string = 'さえちゃん こんにちは'
   return (
+    <>
     <div className="App">
-      <p>ハロー React!</p>
+      <p>{message}</p>
     </div>
+    <ul>
+      { items.map((item: Item) => (
+        <li key={item.id}>{ item.title}</li>
+      ))}
+    </ul>
+    </>
   )
 }
+
+type Item = {
+  id: number
+  title: string
+}
+
+const items: Item[] = [
+  {
+    id: 1,
+    title: '清水'
+  }, {
+    id: 2,
+    title: '稜也'
+  }
+]
+
 
 export default App
